2bd72f805f9869e54ce16d494edb8058417dc531
Sebastian Amariles <sebas932@gmail.com>
2019-11-24 20:58:23 -0500

    Adding production environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  authEndPoint: "http://guybrush.info/wp-json/jwt-auth/v1",
+  wpRestApiEndpoint: "http://guybrush.info/wp-json/wp/v2",
+  pokeApiEndpoint: "http://guybrush.info/wp-json/pokeapi/v1"
 };
